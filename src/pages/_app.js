83f7ea64154f91c9bc58a23023fe4a22684d1f93
ada-designs <alex.adadesigns@gmail.com>
2025-06-23 20:02:23 +0300

    Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,9 @@ import "@/styles/buttons.css";
 import "../styles/rectangle.css";
 
 import { Montserrat } from "next/font/google";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -10,10 +13,49 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
+// 👇 Replace this with your actual GA ID
+const GA_ID = "G-T7QDHKNXZB";
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag?.("config", GA_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <main className={montserrat.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <main className={montserrat.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
